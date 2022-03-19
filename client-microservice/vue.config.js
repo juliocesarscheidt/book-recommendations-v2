f1ec974b60768da2807ad0e78153f12e8b03f2db
juliocesarscheidt <julio.scheidt96@gmail.com>
2022-03-19 20:30:33 -0300

    Implementing client

--- client-microservice/vue.config.js
+++ client-microservice/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/v1': {
-        target: process.env.VUE_APP_API_GATEWAY_CONN_STRING || 'http://172.16.0.3:3080',
+        target: process.env.VUE_APP_API_GATEWAY_CONN_STRING || 'http://127.0.0.1:3080',
         ws: false,
         changeOrigin: true,
         // pathRewrite: {'^/v1': '/v1'},
