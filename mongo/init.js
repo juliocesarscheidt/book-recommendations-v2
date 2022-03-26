0c706d57630313f10d168c485dc9bdb418cdc53f
juliocesarscheidt <julio.scheidt96@gmail.com>
2022-03-26 11:55:51 -0300

    Add default user

--- mongo/init.js
+++ mongo/init.js
@@ -1,10 +1,16 @@
 db.createCollection('user');
 db.createCollection('user_rate');
 
-// // sample user
-// db.user.insertMany([
-//   {
-//   }
-// ]);
+db.user.insertMany([
+  {
+    "name" : "julio",
+    "surname" : "cesar",
+    "email" : "admin@email.com",
+    "phone" : "41995540808",
+    "password" : "$2a$10$osLfeRqhrd5tea2xIXPZ4.Rid2QK6.hqfXP4f/64c4TGRNYQ5FLZ2",
+    "created_at" : new Date(),
+    "updated_at" : new Date(),
+  }
+]);
 
-// db.user.createIndex({ name: -1 })
+db.user.createIndex({ email: -1 })
